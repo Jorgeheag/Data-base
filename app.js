52cdf8285f3770285a5df4444d38f7ca0180e558
jorgeheag <herreraajorgeh@gmail.com>
2022-06-22 22:26:45 -0500

    entregable

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const { Sequelize } = require('sequelize');
+const express = require("express");
+const { Sequelize } = require("sequelize");
 
-
-const { employeesRouter } = require('./usersRoutes/employessRoustes');
+const { employeesRouter } = require("./usersRoutes/employessRoustes");
 //const { postsRouter } = require('./usersRoutes/postRouterEm')
-
+const { Employee } = require('./models/userModelEm');
 //utils
-const { db } = require('./utils/dataBaseUtils');
+const { db } = require("./utils/dataBaseUtils");
 
 // Init express app
 const app = express();
@@ -14,20 +13,20 @@ const app = express();
 app.use(express.json());
 
 // Define endpoints
-app.use('/', employeesRouter);//cambiar
-
+app.use("/", employeesRouter); //cambiar
 
 db.authenticate()
-	.then(() => console.log('Db authenticated'))
-	.catch(err => console.log(err));
+  .then(() => console.log("Db authenticated"))
+  .catch((err) => console.log(err));
 
-db.sync()
-	.then(() => console.log('Db synced'))
-	.catch(err => console.log(err));
+Employee.sync({ force: true })
+  .then(() => console.log("Db synced"))
+  .catch((err) => console.log(err));
 
+//db.sync()
+//	.then(() => console.log('Db synced'))
+//	.catch(err => console.log(err));
 
-
-
-app.listen(5000, ()=>{
-    console.log('Express app running')
-})
+app.listen(5000, () => {
+  console.log("Express app running");
+});
